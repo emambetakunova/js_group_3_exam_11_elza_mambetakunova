6a088d15192258fd67799b7a1c5d2a8415766c5d
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-27 13:07:43 +0600

    added anonimous and regiter user menus

---
+++ exam_11_frontend/src/components/UI/Menus/UserMenu.js
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react';
+import {DropdownItem, DropdownMenu, DropdownToggle, NavItem, NavLink, UncontrolledDropdown} from "reactstrap";
+import {NavLink as RouterNavLink} from "react-router-dom";
+
+const UserMenu = ({user, logout}) => (
+    <Fragment>
+        <NavItem>
+            <NavLink tag={RouterNavLink} to="/posts/new" exact>Add new item</NavLink>
+        </NavItem>
+        <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+                Hello, {user.username}
+            </DropdownToggle>
+            <DropdownMenu right>
+                <DropdownItem onClick={logout}>
+                    Log out
+                </DropdownItem>
+            </DropdownMenu>
+        </UncontrolledDropdown>
+    </Fragment>
+);
+
+export default UserMenu;
