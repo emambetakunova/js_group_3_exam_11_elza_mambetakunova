cb3a360c4f5b63f59f299009a7351997f768e8a2
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-27 13:14:59 +0600

    added all pages

--- exam_11_frontend/src/App.js
+++ exam_11_frontend/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import {Switch, Route, withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import Container from "reactstrap/es/Container";
+import {NotificationContainer} from "react-notifications";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {logOutUser} from "./store/actions/userActions";
+
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+import ItemsBuilder from "./containers/ItemsBuilder/ItemsBuilder"
+import ItemById from "./containers/ItemById/ItemById";
+import NewItem from "./containers/NewItem/NewItem";
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <NotificationContainer/>
+                <header>
+                    <Toolbar
+                        user={this.props.user}
+                        logout={this.props.logOutUser}
+                    />
+                </header>
+                <Container className="my-5">
+                    <Switch>
+                        <Route path="/" exact component={ItemsBuilder}/>
+                        <Route path="/register" exact component={Register}/>
+                        <Route path="/login" exact component={Login}/>
+                        <Route path="/posts/new" exact component={NewItem}/>
+                        <Route path="/posts/:id" component={ItemById}/>
+
+                    </Switch>
+                </Container>
+            </Fragment>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    user: state.user.user
+});
+
+const mapDispatchToProps = dispatch => ({
+    logOutUser: () => dispatch(logOutUser())
+});
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
