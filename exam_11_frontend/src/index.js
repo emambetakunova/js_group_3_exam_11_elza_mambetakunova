2955fa71dbec10faa265a84c56f719f5aa1db9e3
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-27 13:14:36 +0600

    added provider

--- exam_11_frontend/src/index.js
+++ exam_11_frontend/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {ConnectedRouter} from 'connected-react-router';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store, {history} from "./store/configureStore";
+import App from './App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-notifications/lib/notifications.css';
+
+
+const app = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
+
 serviceWorker.unregister();
