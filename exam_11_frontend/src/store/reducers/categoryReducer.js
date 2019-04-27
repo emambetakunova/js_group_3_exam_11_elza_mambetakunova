27027165cc6aea1ad2f5f291017e5c006ba8a6b9
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-27 13:12:21 +0600

    added action and reducer for categories

---
+++ exam_11_frontend/src/store/reducers/categoryReducer.js
@@ -0,0 +1,17 @@
+import {FETCH_CATEGORIES_SUCCESS} from "../actions/categoryActions";
+
+const initialState = {
+    categories: []
+};
+
+
+const categoriesReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_CATEGORIES_SUCCESS:
+            return {...state, categories: action.categories};
+        default:
+            return state;
+    }
+};
+
+export default categoriesReducer;
